07526679fb6d5ef70b0d95e915db02e1542bbd30
AntLouiz <luizrodrigo46@hotmail.com>
2020-09-05 12:57:53 -0300

    Add home page

--- hotel/settings.py
+++ hotel/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    # Third part packages
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = f'{BASE_DIR}/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
